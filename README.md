b5354c68686cf974bca385dffc960b4fb7dab150
Ayodeji Solomon <aa705@greenwich.ac.uk>
2016-04-14 16:43:42 +0100

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+Hide pesky file limit exceeded slackbot messages
---
+++ myscript.js
@@ -0,0 +1,3 @@
+var body = document.body;
+var style = '<style type="text/css" id="customfont">.message.ephemeral{ display:none; }</style>';
+body.insertAdjacentHTML('afterbegin', style);
